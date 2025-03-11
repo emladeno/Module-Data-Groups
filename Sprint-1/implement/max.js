890aff49ea406ddc5862e59751c241b4feebe942
emladeno <mladenova.elvira@yahoo.com>
2025-03-11 11:48:15 +0000

    implementing a function

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,16 @@
 function findMax(elements) {
+  let maxValue = -Infinity;
+
+  for (let item of elements) {
+    if (typeof item === "number" && isFinite(item)) { 
+      if (item > maxValue) {
+        maxValue = item;
+      }
+    }
+  }
+
+  return maxValue;
 }
+
 
 module.exports = findMax;
