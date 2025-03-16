bc14cdee9eab3fe94d39d48c399fa333e994c74b
emladeno <mladenova.elvira@yahoo.com>
2025-03-16 11:14:48 +0000

    implement a function-contains

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,11 @@
-function contains() {}
+function contains(obj,key) {
+if(typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+    throw new Error("Invalid input: must be an object");
+} 
+    return key in obj;
+
+ }
+
+
 
 module.exports = contains;
