a62d5d0971e49d19aec8f130f08046a3609ae27a
emladeno <mladenova.elvira@yahoo.com>
2025-03-16 13:06:48 +0000

    implemented function called "createLookUp

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,25 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+    if (!Array.isArray(countryCurrencyPairs) || 
+      !countryCurrencyPairs.every(pair => Array.isArray(pair))) {
+    throw new Error("Invalid input: Must be an array of arrays.");
+  }
+ const lookup = {};
+
+  for (let pair of countryCurrencyPairs) {
+     if (pair.length !== 2) {
+      throw new Error(`Invalid pair: Each pair must contain exactly two elements. Found: ${JSON.stringify(pair)}`);
+    }
+    const [country, currency] = pair;
+      if (lookup.hasOwnProperty(country)) {
+      throw new Error(`Duplicate country code detected: '${country}'`);
+    }
+    lookup[country] = currency; 
+  }
+
+  return lookup;
 }
+
+
+console.log(createLookup([['US', 'USD'], ['CA', 'CAD']])); 
 
 module.exports = createLookup;
