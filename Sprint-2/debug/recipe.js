aaaefe3024db4ef35eaccdda4895fb95e1ea4da3
emladeno <mladenova.elvira@yahoo.com>
2025-03-16 10:29:37 +0000

    fix the code called(recipe)

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+//it should log the title value and the recipe value but the ingredients-value it is not going to log it and it is not going to log on a new line.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -12,4 +13,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients.join("\n")}`);
