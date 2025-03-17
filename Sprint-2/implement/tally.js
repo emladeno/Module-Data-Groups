79affeb884e57304897a18c5a4f7d2e176abb2be
emladeno <mladenova.elvira@yahoo.com>
2025-03-17 13:29:44 +0000

    implementing a function

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,22 @@
-function tally() {}
+function tally(items) {
+if (!Array.isArray(items)) {
+    throw new Error("Input must be an array");
+  }
+
+  const count = {};
+
+  
+  for (const item of items) {
+    if (count[item]) {
+      count[item]++;
+    } else {
+      count[item] = 1;
+    }
+  }
+
+  return count;
+}
+
+
 
 module.exports = tally;
