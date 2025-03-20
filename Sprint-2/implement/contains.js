f7616eb9eab08921d5e281a40d9627f0d0d620dd
emladeno <mladenova.elvira@yahoo.com>
2025-03-20 20:02:50 +0000

    i have committed new changes so the test pass

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,11 +1,19 @@
-function contains(obj,key) {
-if(typeof obj !== "object" || obj === null || Array.isArray(obj)) {
-    throw new Error("Invalid input: must be an object");
-} 
-    return key in obj;
+function contains(obj, key) {
+if (typeof obj !== 'object' || obj === null) {
+        throw new Error('Invalid input: must be an object');
+    }
+    
+    if (Array.isArray(obj)) {
+        return false;
+    }
+    
+    if (typeof key !== 'string') {
+        return false;
+    }
+    
 
- }
-
+    return obj.hasOwnProperty(key);
+}
 
 
 module.exports = contains;
