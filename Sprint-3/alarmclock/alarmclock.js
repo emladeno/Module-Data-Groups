c4faa4e493ee7fb988d4f97c9533d6563d0aaf67
emladeno <mladenova.elvira@yahoo.com>
2025-03-21 10:34:08 +0000

    implementing a function

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,43 @@
-function setAlarm() {}
+function startCountdown() {
+  const input = document.querySelector('#alarmSet');
+  const heading = document.querySelector('#timeRemaining');
+  let timeRemaining = parseInt(input.value);
+  let countdown;
+
+  function updateDisplay() {
+    const minutes = String(Math.floor(timeRemaining / 60)).padStart(2, '0');
+    const seconds = String(timeRemaining % 60).padStart(2, '0');
+    heading.innerText = `Time Remaining: ${minutes}:${seconds}`;
+  }
+
+  function startTimer() {
+    clearInterval(countdown);
+    timeRemaining = parseInt(input.value);
+    updateDisplay();
+
+    countdown = setInterval(() => {
+      if (timeRemaining <= 0) {
+        clearInterval(countdown);
+        playAlarm();
+      } else {
+        timeRemaining--;
+        updateDisplay();
+      }
+    }, 1000);
+  }
+
+  document.querySelector('#set').addEventListener('click', startTimer);
+
+  updateDisplay();
+}
+
+function playAlarm() {
+  const audio = new Audio('alarm.mp3');
+  audio.play();
+}
+
+startCountdown();
+
 
 // DO NOT EDIT BELOW HERE
 
