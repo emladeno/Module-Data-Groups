eda39ce3447d47ee856d7fee9d6cafe0c234838e
emladeno <mladenova.elvira@yahoo.com>
2025-06-30 19:54:54 +0100

    adding more to the function

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -3,23 +3,23 @@
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
 
 function calculateMedian(list) {
-  if (!list.length) {
-    throw new Error ("Array can not be empty.");
-  }
+  if (!Array.isArray(list)) return null;
+
+  const numbers = list.filter(
+    (item) => typeof item === "number" && !isNaN(item)
+  );
+  if (numbers.length === 0) return null;
+
   const sortedNumbers = [...list].sort((a, b) => a - b);
   const middleIndex = Math.floor(sortedNumbers.length / 2);
-  
-  if (sortedNumbers.length % 2 === 1 ) {
- return sortedNumbers [middleIndex];
-  }
-  else {
-   const middle1 = sortedNumbers [middleIndex - 1];
-   const middle2 = sortedNumbers [middleIndex];
-   return (middle1 + middle2) / 2;
-  }
 
-  
+  if (sortedNumbers.length % 2 === 1) {
+    return sortedNumbers[middleIndex];
+  } else {
+    const middle1 = sortedNumbers[middleIndex - 1];
+    const middle2 = sortedNumbers[middleIndex];
+    return (middle1 + middle2) / 2;
+  }
 }
-    
 
 module.exports = calculateMedian;
