57e405f44497b5269ee3edcec0fcdbd876251606
CJ Yuan <cjyuan00@gmail.com>
2025-07-07 00:36:08 +0100

    Add test cases to check if array is sorted by numeric values
    Also changed the array in "doesn't modify the input array" to an unsorted array.

--- Sprint-1/fix/median.test.js
+++ Sprint-1/fix/median.test.js
@@ -21,14 +21,16 @@ describe("calculateMedian", () => {
     { input: [5, 1, 3, 4, 2], expected: 3 },
     { input: [4, 2, 1, 3], expected: 2.5 },
     { input: [6, 1, 5, 3, 2, 4], expected: 3.5 },
+    { input: [110, 20, 0], expected: 20 },
+    { input: [6, -2, 2, 12, 14], expected: 6 },
   ].forEach(({ input, expected }) =>
     it(`returns the correct median for unsorted array [${input}]`, () => expect(calculateMedian(input)).toEqual(expected))
   );
 
-  it("doesn't modify the input array [1, 2, 3]", () => {
-    const list = [1, 2, 3];
+  it("doesn't modify the input array [3, 1, 2]", () => {
+    const list = [3, 1, 2];
     calculateMedian(list);
-    expect(list).toEqual([1, 2, 3]);
+    expect(list).toEqual([3, 1, 2]);
   });
 
   [ 'not an array', 123, null, undefined, {}, [], ["apple", null, undefined] ].forEach(val =>
